810cfd7e1d207af7dc0533a6a3efd44e23e32ac9
Jack Curtis <jackcurt.com@gmail.com>
2025-09-24 15:18:17 -0400

    cleaned up somme code

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -3,18 +3,23 @@ extends Node2D
 var bullet_scene = preload("res://scenes/bullet.tscn")
 var bullet_speed = 1000
 
+@onready var pivot = $Pivot
+@onready var barrel = $Pivot/Barrel
+@onready var laser = $Laser
+@onready var laser_cast = $Pivot/Barrel/LaserPoint
+
 func _physics_process(delta):
-	$Pivot.look_at(get_global_mouse_position())
-	var barrelPos = $Pivot/Barrel.global_position
+	pivot.look_at(get_global_mouse_position())
+	var barrelPos = barrel.global_position
 	barrelPos.x -= 16
-	$Laser.set_point_position(0, to_local(barrelPos))
+	laser.set_point_position(0, to_local(barrelPos))
 	if Input.is_action_just_pressed("click"):
 		fire()
-	if $Pivot/Barrel/LaserPoint.is_colliding():
-		$CollisionPoint.global_position = $Pivot/Barrel/LaserPoint.get_collision_point()
-		var colPos = $Pivot/Barrel/LaserPoint.get_collision_point()
+	if laser_cast.is_colliding():
+		$CollisionPoint.global_position = laser_cast.get_collision_point()
+		var colPos = laser_cast.get_collision_point()
 		colPos.x -= 16
-		$Laser.set_point_position(1, to_local(colPos))
+		laser.set_point_position(1, to_local(colPos))
 		
 	
 		
@@ -22,6 +27,6 @@ func fire():
 	print("gun fired")
 	
 	var bullet = bullet_scene.instantiate()
-	bullet.position = $Pivot/Barrel.global_position # make bullet position global_pos of barrel
-	bullet.apply_impulse(Vector2(bullet_speed,0).rotated($Pivot.global_rotation)) # apply velocity to global position of barrel
+	bullet.position = barrel.global_position # make bullet position global_pos of barrel
+	bullet.apply_impulse(Vector2(bullet_speed,0).rotated(pivot.global_rotation)) # apply velocity to global position of barrel
 	get_tree().current_scene.add_child(bullet) # parent bullet to SCENE
