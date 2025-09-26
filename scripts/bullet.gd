47014e891be23b990428150a9a49b1e45a129a11
Jack Curtis <jackcurt.com@gmail.com>
2025-09-26 10:32:00 -0400

    added freeze position if bullet reaches end

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,6 @@
 extends PathFollow2D
 
-const BULLET_SPEED = 3000
+const BULLET_SPEED = 1000
 
 signal path_completed
 
@@ -9,6 +9,7 @@ var old_progress = 0
 func _process(delta: float) -> void:
 	progress += BULLET_SPEED*delta
 	if progress - old_progress < 0:
+		progress -= BULLET_SPEED*delta
 		path_completed.emit()
 	old_progress = progress
 	print(progress)
