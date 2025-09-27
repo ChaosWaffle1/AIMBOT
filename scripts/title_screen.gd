640de884bb53a1ee51c0d5ed4a72aabc8b998692
nicholasryanwagner <nicholasryanwagner@gmail.com>
2025-09-27 13:20:05 -0400

    Main Menu!

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -11,3 +11,7 @@ func _on_credits_button_up() -> void:
 
 func _on_version_notes_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/scroll_notes.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
