34d0dba677fe8cdf99de293a893174904d31680e
Jack Curtis <jackcurt.com@gmail.com>
2025-10-01 10:44:01 -0400

    renamed bullet_path.gd to bullet.gd. stilll trying to figure out why not working :(

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-const SLOP = 0.999
+const SLOP = 0.9999
 
 var bounces: int = 0
 var finished: bool = false
 
-@export var bullet_speed: float = 50000
-@export var max_bounces: int = 20
+@export var bullet_speed: float = 50
+@export var max_bounces: int = 2
 
 @onready var path: Path2D = $Path
 @onready var path_follow: PathFollow2D = $Path/PathFollow
@@ -26,18 +26,27 @@ func _ready() -> void:
 	debug_line.add_point(to_local(sight.global_position))
 
 func _physics_process(delta: float) -> void:
-	#if not $delay.is_stopped():
-		#return
-	if path.curve.point_count > 1000:
+	if path.curve.point_count > 500:
 		queue_free()
 		return
+		
 	if bounces >= max_bounces:
 		if kill_switch.is_stopped():
 			kill_switch.start()
 		return
+		
 	var distance_remaining: float = bullet_speed * delta
-	sight.target_position = (bullet_speed*delta) * sight.target_position.normalized()
 	var ray: RayCast2D = sight
+
+	ray.force_raycast_update()
+	if (ray.global_position + ray.target_position).x < -144:
+		pass
+
+	print(str(ray.global_position) + " " + str(ray.global_position + ray.target_position))
+	print(ray.is_colliding())
+	
+	#TODO: WHY DOES THIS NOT REGISTER UNTIL THE NEXT FRAME??????????
+	
 	while bounces < max_bounces:			
 		ray.force_raycast_update() # why do i need this here :(
 		if not ray.is_colliding():
@@ -46,11 +55,19 @@ func _physics_process(delta: float) -> void:
 			debug_line.add_point(path.to_local(ray.global_position) + ray.target_position)
 			path_follow.progress += SLOP*distance_remaining
 			return
-			
+		
+		# ray is colliding from this point on
 		bounces += 1
+		
 		# add hit to path
 		var hit: Vector2 = ray.get_collision_point()
 		var normal: Vector2 = ray.get_collision_normal()
+		#
+		#print("bounces:\t" + str(bounces))
+		#print("hit:\t\t" + str(hit))
+		#print("normal:\t\t" + str(normal)) #WHY IS THE NORMAL VECTOR WRONG
+		#print("target:\t\t" + str(ray.target_position))
+		
 		path.curve.add_point(path.to_local(hit))
 		debug_line.add_point(path.to_local(hit))
 		
@@ -58,7 +75,7 @@ func _physics_process(delta: float) -> void:
 		var distance_to_hit = (ray.position - ray.to_local(hit)).length()
 		path_follow.progress += SLOP*distance_to_hit
 		distance_remaining -= SLOP*distance_to_hit
-
+		
 		# create a new ray
 		var new_ray = RayCast2D.new()
 		ray.enabled = true
